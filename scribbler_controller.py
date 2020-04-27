10d5bb06e1925850cdfaa44b6417d2d24eaf9fed
Kyle Rathman <krathman257@gmail.com>
2020-04-27 11:11:54 -0600

    Added pen, touched up scribbler

--- scribbler_controller.py
+++ scribbler_controller.py
@@ -1,20 +1,14 @@
-from controller import Robot
+from controller import Robot, Pen
 import math
 
-#Time in milliseconds of a simulation step
+#Initialize variables
 TIME_STEP = 64
+MAX_SPEED = 7
 
 #Create Robot instance
 robot = Robot()
 
 #Initialize devices
-ds = []
-dsNames = ['ds_right', 'ds_left']
-
-for i in range(2):
-    ds.append(robot.getDistanceSensor(dsNames[i]))
-    ds[i].enable(TIME_STEP)
-    
 wheels = []
 wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
 
@@ -23,23 +17,28 @@ for i in range(4):
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
 
+pen = Pen('pen')
+pen.write(True)
+
 ticker = 0
 
 #Feedback loop: Step simulation until receiving an exit event
 while robot.step(TIME_STEP) != -1:
 
-    #Step ticker
+    #Calculate cosine wave, step ticker
     motorAngle = math.cos(ticker/10)
     ticker += 1
 
     #Initialize motor speeds
-    leftSpeed = 1.0
-    rightSpeed = 1.0
+    leftSpeed = 0.5 * MAX_SPEED
+    rightSpeed = 0.5 * MAX_SPEED
 
-    #If between threshold, calculate cosine wave
-    if ticker > 32 and ticker < 224:
-        leftSpeed += motorAngle * 0.5 * MAX_SPEED
-        rightSpeed += -1 * motorAngle * 0.5 * MAX_SPEED
+    #If between threshold, scribble
+    if ticker > 32 and ticker < 285:
+        leftSpeed += -1 * motorAngle * 0.5 * MAX_SPEED
+        rightSpeed += motorAngle * 0.5 * MAX_SPEED
+    if ticker > 320:
+        break
 
     #Write motor speeds
     wheels[0].setVelocity(leftSpeed)
